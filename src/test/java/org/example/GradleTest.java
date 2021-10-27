17e3729a9ce70e16f2fd29dc90215c50831bc830
Daniel Rodriguez Hernandez <daniel.rodriguezhernandez@datadoghq.com>
2021-10-27 16:25:08 +0200

    first commit

---
+++ src/test/java/org/example/GradleTest.java
@@ -0,0 +1,14 @@
+package org.example;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+public class GradleTest {
+
+    @Test
+    public void testTrue(){
+        assertTrue(true);
+    }
+
+}
